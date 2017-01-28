1668999b7bd8f26b1df0181750a508fe1ce556cb
Jan Hieber <mail@janhieber.net>
2017-01-28 23:03:38 +0100

    update

--- home/.bin/polybar_laptop.sh
+++ home/.bin/polybar_laptop.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-killall -q polybar                                                               
-while pgrep -x polybar >/dev/null; do sleep 0.5; done                            
-                                                                                 
-MONITOR=eDP-1 polybar -r -c ~/.config/polybar/config_$(hostname) main &                                                
-MONITOR=HDMI-1 polybar -r -c ~/.config/polybar/config_$(hostname) main &
+# kill all bars
+killall -q polybar
+while pgrep -x polybar >/dev/null; do sleep 0.5; done
+
+CFG="$HOME/.config/polybar/config_$(hostname)"
+
+# launch a polybar first eDP-1 to get tray
+MONITOR='eDP-1' polybar -c $CFG main &
+
+# get connected displays and launch a bar for each
+polybar -m | cut -d' ' -f1 | sed 's/://' |\
+while read line; do
+  [ "$line" != "eDP-1" ] && \
+    MONITOR="$line" polybar -c $CFG main &
+done
 
--- startup_laptop.sh
+++ startup_laptop.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
+# setup displays
 autorandr -c --default default &
+
+# do xorg stuff
 xrdb -load .Xresources &
-numlockx on &
-nitrogen --restore &
 xset r rate 250 45
+xrandr --newmode '1920x1080_60.00' 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode DP-2 '1920x1080_60.00'
+xrandr --newmode '1920x1200_60.00' 193.25 1920 2056 2256 2592 1200 1203 1209 1245 -hsync +vsync
+xrandr --addmode DP-2 '1920x1200_60.00'
+numlockx on &
+compton -b
+
+# launch some apps
 nm-applet &
 nextcloud &
 dunst &
-compton -b
 urxvtd -f
+
+# set bg and launch bars
 feh --no-fehbg --bg-tile ~/dotfiles/bg.png
-~/.bin/polybar_.sh
+~/.bin/polybar_$(hostname).sh
+xeventbind resolution ~/.bin/polybar_$(hostname).sh &
 
