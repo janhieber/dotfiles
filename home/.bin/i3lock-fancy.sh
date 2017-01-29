75c646ea2918464513d8b3cf0ea9eecd8065210e
Jan Hieber <mail@janhieber.net>
2017-01-29 21:12:36 +0100

    update

---
+++ home/.bin/i3lock-fancy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+SIGMA=2.0
+
+IMAGE=$(mktemp /tmp/i3lock_XXX.jpg)
+scrot -z $IMAGE
+convert $IMAGE -filter Gaussian -resize 25% -define filter:sigma=$SIGMA -resize 400% ${IMAGE%.jpg}.png
+rm $IMAGE
+i3lock -i ${IMAGE%.jpg}.png -t -I 10 -u -e
+rm ${IMAGE%.jpg}.png
+
