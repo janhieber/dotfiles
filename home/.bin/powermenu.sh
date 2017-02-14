038b80ca424414e6d59dc7815bf4048039dcc3d1
Jan Hieber <mail@janhieber.net>
2017-02-15 00:32:55 +0100

    update

---
+++ home/.bin/powermenu.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+res=$(echo "lock|suspend|hibernate|reboot|shutdown|logout" | rofi -sep "|" -dmenu -i -p 'Power Menu: ' "" -auto-select)
+
+function sure {
+  local res=$(echo "no|yes" | rofi -sep "|" -dmenu -i -p "$1: sure?" -auto-select)
+  [[ $res = "yes" ]] && exec $2
+}
+
+[[ $res = "suspend" ]]   && systemctl suspend
+[[ $res = "lock" ]]      && ~/.bin/i3lock-fancy.sh
+[[ $res = "logout" ]]    && sure $res 'i3-msg exit'
+[[ $res = "reboot" ]]    && sure $res 'systemctl reboot'
+[[ $res = "shutdown" ]]  && sure $res 'systemctl poweroff'
+[[ $res = "hibernate" ]] && sure %res 'systemctl hibernate'
+
+exit 0
+
--- startup_laptop.sh
+++ startup_laptop.sh
@@ -6,6 +6,7 @@ autorandr -c --default default &
 # do xorg stuff
 xrdb -load .Xresources &
 xset r rate 250 45
+xset s 3600 3600
 xrandr --newmode '1920x1080_60.00' 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode DP-2 '1920x1080_60.00'
 xrandr --newmode '1920x1200_60.00' 193.25 1920 2056 2256 2592 1200 1203 1209 1245 -hsync +vsync
