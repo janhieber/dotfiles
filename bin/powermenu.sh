33cd594099e2edd70b16e4bb976d264f1eb3049e
Jan Hieber <mail@janhieber.net>
2017-07-26 22:52:52 +0200

    update

--- bin/powermenu.sh
+++ bin/powermenu.sh
@@ -9,7 +9,8 @@ function sure {
 
 [[ $res = "suspend" ]]   && systemctl suspend
 [[ $res = "suspend undock" ]]   && autorandr -l default && systemctl suspend
-[[ $res = "lock" ]]      && ~/bin/i3lock-fancy.sh
+[[ $res = "lock" ]]      && i3lock -i ~/.wallpaper -c 000000 -e
+#[[ $res = "lock" ]]      && ~/bin/i3lock-fancy.sh
 [[ $res = "logout" ]]    && sure $res 'i3-msg exit'
 [[ $res = "reboot" ]]    && sure $res 'systemctl reboot'
 [[ $res = "reboot to windows" ]]    && sure "$res" 'sudo /usr/local/bin/reboot-windows'
